19bc729412c17426d3fe0dd1654895407cba36fd
Nikolas Mikolaski <mikolaskin@gmail.com>
2022-08-08 13:08:57 -0400

    bubble sort does multiple passes to fully sort array.

--- main.cpp
+++ main.cpp
@@ -4,20 +4,30 @@
 void main()
 {
 	int unsortedArr[] = { 7, 3, 4, 2, 10, 67, 2, 1 };
+	
+	bool sorted;
 
-	for (int i = 0; i < std::size(unsortedArr) - 1; i++)
+	do
 	{
-		if (unsortedArr[i] > unsortedArr[i + 1])
+		sorted = false;
+
+		for (int i = 0; i < std::size(unsortedArr) - 1; i++)
 		{
-			int temp = unsortedArr[i];
-			unsortedArr[i] = unsortedArr[i + 1];
-			unsortedArr[i + 1] = temp;
+
+			if (unsortedArr[i] > unsortedArr[i + 1])
+			{
+				sorted = true;
+				int temp = unsortedArr[i];
+				unsortedArr[i] = unsortedArr[i + 1];
+				unsortedArr[i + 1] = temp;
+			}
+
 		}
-	}
+	} while (sorted);
+
 	for (int i = 0; i < std::size(unsortedArr); i++)
 	{
 		std::cout << unsortedArr[i] << " ";
 	}
 }
-
 
